fda8afe19fbaee7eb3a73382f7200982823b6417
roverelk <eirik.kjevik@gmail.com>
2017-09-30 13:09:30 +0200

    Startet min første branch!

---
+++ Preferansevalg_2vinnere.py
@@ -0,0 +1,10 @@
+# Når en får to eller flere som vinner.
+# Se på andrestemmen de gitte kandidatene for å velge en som vinner.
+# Om to eller flere har like mange andrestemmer, gå videre og tell tredjestemmene.
+
+# Siden dette kna skje når so helst må det være dynamisk. Dvs at den som telles over, må se på de _neste_ stemmene på
+# de gitte stemmesedlene.
+
+# Om en av de to gjennværende kandidatene ikke har flere stemmer, har den personen tapt.
+
+# Om to eller flere kandidater har like mange stemmer i alle ledd. Velges det med myntkast.
